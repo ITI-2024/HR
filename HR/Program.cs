0a34377264eec0a37525917c149db61bc261dbc1
ANASGROUP <ANASGROUP@HABIBAELHADARY>
2024-04-25 01:24:17 +0200

    testhabibA

--- HR/Program.cs
+++ HR/Program.cs
@@ -8,12 +8,11 @@ namespace HR
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            //habibad
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
